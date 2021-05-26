9ee08535b8e2da9bad3ddf675a18bb7dabeb33ac
Wendel Lopes <wendelopes@msn.com>
2021-05-26 08:37:16 -0300

    Layout project with style css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Calculator from './main/Calculator'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1 className='title is-4'>Calculadora</h1>
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
